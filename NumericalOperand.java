9eb210bf4e125e0262ce8127cdb6aecab91438c8
Alirsgp <Alirsgp@gmail.com>
2017-02-14 19:42:13 -0500

    Create NumericalOperand.java

---
+++ NumericalOperand.java
@@ -0,0 +1,6 @@
+public interface NumericalOperand {
+	 public NumericalOperand add(NumericalOperand operand);
+	 public NumericalOperand subtract(NumericalOperand operand);
+
+	//public double area(double length, double width, double height); Ignore this
+}
