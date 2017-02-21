9677dfad579f35d080d15f576876e9367462e322
Alirsgp <Alirsgp@gmail.com>
2017-02-21 03:44:41 -0500

    Update NumericalOperand.java

--- NumericalOperand.java
+++ NumericalOperand.java
@@ -1,6 +1,8 @@
+//Ali Mohammadian - CSC 202 - Given interface
+
 public interface NumericalOperand {
 	 public NumericalOperand add(NumericalOperand operand);
 	 public NumericalOperand subtract(NumericalOperand operand);
 
-	//public double area(double length, double width, double height); Ignore this
+	
 }
